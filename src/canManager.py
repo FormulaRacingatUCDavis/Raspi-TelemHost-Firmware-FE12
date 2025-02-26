d42546eb1241d544bbeb59f1d72f17ae974597b2
Ryan Tengan <ryantengan2004@gmail.com>
2025-02-26 05:46:20 +0000

    added .gitignore and canManager w/ CAN index

---
+++ src/canManager.py
@@ -0,0 +1,33 @@
+'''
+
+Speed: Front wheels
+> CAN ID: 500
+> Bytes 5-2
+
+Vehicle State
+> CAN ID: 766
+> Byte 5
+
+GLV Voltage (MC_INTERNAL_VOLTS)
+> CAN ID: A9
+> Bytes 7-6
+
+Motor Temperature (motor_temp)
+> CAN ID: A2
+> Bytes 5-4
+
+Motor Controller Temperature (mc_temp): Average of Modules A, B, and C
+> CAN ID: A0
+> Module A: Bytes 1-0
+> Module B: Bytes 3-2
+> Module C: Bytes 5-4
+
+Battery Temperature (PACK_TEMP)
+> CAN ID: 380
+> Byte 0
+
+State of charge
+> CAN ID: 380
+> Byte 1
+
+'''
