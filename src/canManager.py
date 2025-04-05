2f763aaf63c42afd3e6099647535505fe672758c
Ryan Tengan <ryantengan2004@gmail.com>
2025-04-05 10:03:52 +0100

    fixed CAN manager / added different color for different cases

--- src/canManager.py
+++ src/canManager.py
@@ -1,124 +1,116 @@
-'''
-
-CAN INDEX:
-
-Speed: Front wheels
-> CAN ID: 500
-> Bytes 5-2
-
-Vehicle State
-> CAN ID: 766
-> Byte 5
-
-GLV Voltage (MC_INTERNAL_VOLTS)
-> CAN ID: A9
-> Bytes 7-6
-
-Motor Temperature (motor_temp)
-> CAN ID: A2
-> Bytes 5-4
-
-Motor Controller Temperature (mc_temp): Average of Modules A, B, and C
-> CAN ID: A0
-> Module A: Bytes 1-0
-> Module B: Bytes 3-2
-> Module C: Bytes 5-4
-
-Battery Temperature (PACK_TEMP)
-> CAN ID: 380
-> Byte 0
-
-State of charge
-> CAN ID: 380
-> Byte 1
-
-'''
-
 import can
 
 class FE12CANBus:
     def __init__(self, channel, interface):
+        
+        print("Instantiating CAN manager...")
+
+        self.canID = -1
+        
         self.speed = -1
-        self.state = -1
+        self.vcuState = -1
+        self.bmsState = -1
         self.glvVoltage = -1
         self.motorTemp = -1
         self.mcTemp = -1
         self.packTemp = -1
         self.soc = -1
 
-        self.stateMessage = {
-            '0x00': 'LV',
-            '0x01': 'PRECHARGE',
-            '0x02': 'HV',
-            '0x03': 'DRIVE',
-            '0x05': 'STARTUP',
-            '0x81': 'DRV FRM LV',           # FAULT: Drive request from LV
-            '0x82': 'PRE TM OUT',             # FAULT: Precharge timeout
-            '0x83': 'BR NOT PRS',             # FAULT: Brake not pressed
-            '0x84': 'HV OFF DRV',           # FAULT: HV disabled while driving
-            '0x85': 'SNSR DSCRP',            # FAULT: Sensor discrepancy
-            '0x86': 'BSPD TRIPD',             # FAULT: BSPD tripped
-            '0x87': 'SHTDWN OPEN',              # FAULT: Shutdown Circuit Open
-            '0x88': 'UNCALIBRTD',           # FAULT: Uncalibrated
-            '0x89': 'HARD BSPD',             # FAULT: Hard BSPD
-            '0x8A': 'MC FAULT'               # FAULT: MC Fault
-        }
-
         self.idHandlers = {
             '0x500': self.handleSpeed,      # Speed [mph]
-            '0x766': self.handleState,      # Vehicle state [LV, Precharge, HV, Drive, ...]
-            '0xA9': self.handleGLVVoltage,  # GLV voltage [V]
-            '0xA2': self.handleMotorTemp,   # Motor temperature [C]
-            '0xA0': self.handleMCTemp,      # Motor controller temperature; average [C]
+            '0x766': self.handleVCUState,      # Vehicle state [LV, Precharge, HV, Drive, ...]
+            '0x380': self.handleBMSState,
+            '0xa9': self.handleGLVVoltage,  # GLV voltage [V]
+            '0xa2': self.handleMotorTemp,   # Motor temperature [C]
+            '0xa0': self.handleMCTemp,      # Motor controller temperature; average [C]
             '0x380': self.handlePackTemp    # Battery temperature [C] and State of Charge [%]
         }
 
         self.bus = can.interface.Bus(channel=channel, interface=interface)
 
-    def handleSpeed(self, msg):
-        if not self.lenError(msg, 3):
-            self.speed = int.from_bytes(msg.data[3:2:-1], "big")
-
-    def handleState(self, msg):
-        if not self.lenError(msg, 5):
-            try:
-                self.state = self.stateMessage[hex(msg.data[5])]
-            except:
-                print(f"Invalid state encoding: {hex(msg.data[5])}: {msg}")
-
-    def handleGLVVoltage(self, msg):
-        if not self.lenError(msg, 8):
-            self.glvVoltage = int.from_bytes(msg.data[8:6:-1], "big")
-
-    def handleMotorTemp(self, msg):
-        if not self.lenError(msg, 5):
-            self.motorTemp = int.from_bytes(msg.data[5:3:-1], "big")
-
-    def handleMCTemp(self, msg):
-        if not self.lenError(msg, 5):
-            self.mcTemp = sum((
-            int.from_bytes(msg.data[1::-1], "big"),         # Module A
-            int.from_bytes(msg.data[3:1:-1], "big"),        # Module B
-            int.from_bytes(msg.data[5:3:-1], "big")         # Module C
-            )) / 3
-
-    def handlePackTemp(self, msg):
-        if not self.lenError(msg, 0):
-            self.packTemp = msg.data[0]
-    
-    def handleSOC(self, msg):
-        if not self.lenError(msg, 1):
-            self.soc = msg.data[1]
-
     def readMsg(self):
+        print("Reading messsage...")
+
         msg = self.bus.recv()
-        canID = hex(msg.arbitration_id)
-        if canID in self.idHandlers:
-            self.idHandlers[canID](msg)
+        self.canID = hex(msg.arbitration_id)
+
+        if self.canID in self.idHandlers:
+            self.idHandlers[self.canID](msg)
         else:
-            print(f"\nInvalid CAN ID ({canID}): {msg}")
+            print(f"\nInvalid CAN ID ({self.canID}): {msg}")
 
     def lenError(self, msg, size):
         if len(msg) < size:
             print(f"\nData length requirement unmet: {msg.data}. Expected at least {size} bytes: {msg}")
+
         return len(msg) < size
+
+# Speed: Front wheels
+# CAN ID: 500
+# Bytes 2-5
+
+    def handleSpeed(self, msg):
+        if not self.lenError(msg, 3):
+            self.speed = int.from_bytes(msg.data[2:6], "big")
+
+# Vehicle State
+# CAN ID: 766
+# Byte 5
+
+    def handleVCUState(self, msg):
+        if not self.lenError(msg, 5):
+            self.vcuState = msg.data[5]
+
+# BMS State
+# CAN ID: 380
+# Byte 0 (?)
+
+    def handleBMSState(self, msg):
+        if not self.lenError(msg, 0):
+            self.bmsState = msg.data[0]
+
+# GLV Voltage (MC_INTERNAL_VOLTS)
+# CAN ID: A9
+# Bytes 6-7
+
+    def handleGLVVoltage(self, msg):
+        if not self.lenError(msg, 8):
+            self.glvVoltage = int.from_bytes(msg.data[6:8], "big")
+
+# Motor Temperature (motor_temp)
+# CAN ID: A2
+# Bytes 4-5
+
+    def handleMotorTemp(self, msg):
+        if not self.lenError(msg, 5):
+            self.motorTemp = int.from_bytes(msg.data[4:6], "big")
+
+# Motor Controller Temperature (mc_temp): Average of Modules A, B, and C
+# CAN ID: A0
+# Module A: Bytes 0-1
+# Module B: Bytes 2-3
+# Module C: Bytes 4-5
+
+    def handleMCTemp(self, msg):
+        if not self.lenError(msg, 5):
+            self.mcTemp = sum((
+            int.from_bytes(msg.data[0:2], "big"),         # Module A
+            int.from_bytes(msg.data[2:4], "big"),        # Module B
+            int.from_bytes(msg.data[4:6], "big")         # Module C
+            )) / 3
+
+# Battery Temperature (PACK_TEMP)
+# CAN ID: 380
+# Byte 0
+
+    def handlePackTemp(self, msg):
+        if not self.lenError(msg, 0):
+            self.packTemp = msg.data[0]
+
+# State of charge
+# CAN ID: 380
+# Byte 1
+    
+    def handleSOC(self, msg):
+        if not self.lenError(msg, 1):
+            self.soc = msg.data[1]
--- src/dashboard.py
+++ src/dashboard.py
@@ -2,101 +2,192 @@
 
 # FE12 Dashboard
 # Display data received from Raspberry Pi telemetry host
-# Note: GUI currently does NOT display until it receives CAN data
 
 import tkinter as tk
 from canManager import FE12CANBus
 
 class FE12Dashboard:
     def __init__(self, master, channel, interface):
+
+        print("Opening up dashboard...")
+
         self.canbus = FE12CANBus(channel, interface)
         
         self.master = master
         self.master.title("FE12 Dashboard")
         self.master.configure(bg="black")
-        self.master.geometry("1980x1080")
 
         self.dashboard = tk.Frame(self.master, bg="black")
         self.dashboard.pack(fill="both", expand=True, pady=20)
 
-        self.dashboard.grid_rowconfigure(0, weight=4, uniform="equal")  # Header
+        self.dashboard.grid_rowconfigure(0, weight=5, uniform="equal")  # Header
 
         self.dashboard.grid_rowconfigure(1, weight=10, uniform="equal") 
         self.dashboard.grid_rowconfigure(2, weight=10, uniform="equal")
 
-        self.dashboard.grid_rowconfigure(3, weight=4, uniform="equal")  # Header
-        self.dashboard.grid_rowconfigure(4, weight=10, uniform="equal")
+        self.dashboard.grid_rowconfigure(3, weight=6, uniform="equal")  # Header
+        self.dashboard.grid_rowconfigure(4, weight=20, uniform="equal")
+        
+        self.dashboard.grid_columnconfigure(0, weight=15, uniform="equal")
+        self.dashboard.grid_columnconfigure(1, weight=1, uniform="equal") # Column divider
+        self.dashboard.grid_columnconfigure(2, weight=15, uniform="equal")
 
-        self.dashboard.grid_columnconfigure(0, weight=10, uniform="equal")
-        self.dashboard.grid_columnconfigure(1, weight=10, uniform="equal")
+        self.padxOut = 10
 
-        self.padxOut = 100
-        self.padxIn = 50
+        self.headerFontSize = 20
 
-        print("Creating widgets...")
+        self.FE_green = "#20e848"
 
         self.createWidgets()
 
-        print("Waiting for CAN messages...")
+        # VCU states
 
-        self.updateDashboard()
+        self.LV = 0x0
+        self.PRECHARGE = 0x1
+        self.HV = 0x2
+        self.DRIVE = 0x3
+        self.STARTUP = 0x5
+        self.DRIVE_REQUEST_FROM_LV = 0x81
+        self.PRECHARGE_TIMEOUT = 0x82
+        self.BRAKE_NOT_PRESSED = 0x83
+        self.HV_DISABLED_WHILE_DRIVING = 0x84
+        self.SENSOR_DISCREPANCY = 0x85
+        self.BRAKE_IMPLAUSIBLE = 0x86
+        self.SHUTDOWN_CIRCUIT_OPEN = 0x87
+        self.UNCALIBRATED = 0x88
+        self.HARD_BSPD = 0x89
+        self.MC_FAULT = 0x8a
 
     def createWidgets(self):
+
         # Speed
-        self.headerSpeed = tk.Label(self.dashboard, text=f"MPH", font=("Trebuchet MS", 40), bg="black", fg="yellow", anchor="s", padx=5, pady=5)
-        self.headerSpeed.grid(row=0, column=0, sticky="nsew", padx=(self.padxOut,self.padxIn))
-        self.lblSpeed = tk.Label(self.dashboard, text=f"...", font=("Trebuchet MS", 150), bg="#ac75d9", fg="black", anchor="center", padx=5, pady=5)
-        self.lblSpeed.grid(row=1, column=0, sticky="nsew", rowspan=2, padx=(self.padxOut,self.padxIn))
+        self.headerSpeed = tk.Label(self.dashboard, text=f"MPH", font=("Trebuchet MS", self.headerFontSize), bg="black", fg="yellow", anchor="s", padx=5, pady=5)
+        self.headerSpeed.grid(row=0, column=0, sticky="nsew", padx=(self.padxOut, 0))
+        self.lblSpeed = tk.Label(self.dashboard, text=f"...", font=("Trebuchet MS", 75), bg="#ac75d9", fg="black", anchor="center", padx=5, pady=5)
+        self.lblSpeed.grid(row=1, column=0, sticky="nsew", rowspan=2, padx=(self.padxOut, 0))
 
         # Vehicle state
-        self.headerState = tk.Label(self.dashboard, text=f"STATE:", font=("Trebuchet MS", 40), bg="black", fg="yellow", anchor="w", pady=5)
-        self.headerState.grid(row=3, column=0, sticky="nsew", padx=(self.padxOut,self.padxIn))
-        self.lblState = tk.Label(self.dashboard, text=f"...", font=("Trebuchet MS", 70), bg="yellow", fg="black", anchor="center", padx=5, pady=5)
-        self.lblState.grid(row=4, column=0, sticky="nsew", padx=(self.padxOut,self.padxIn))
+        self.headerState = tk.Label(self.dashboard, text=f"STATE:", font=("Trebuchet MS", self.headerFontSize), bg="black", fg="yellow", anchor="w", pady=5)
+        self.headerState.grid(row=3, column=0, sticky="nsew", padx=(self.padxOut, 0))
+        self.lblState = tk.Label(self.dashboard, text=f"...", font=("Trebuchet MS", 35), bg="yellow", fg="black", anchor="center", padx=5, pady=5)
+        self.lblState.grid(row=4, column=0, sticky="nsew", padx=(self.padxOut, 0))
 
-        FE_green = "#20e848"
+        # Column Divider
+        self.colDivider = tk.Frame(self.dashboard, bg ="black")
+        self.colDivider.grid(column= 1, sticky="nsew", rowspan=2)
 
         # GLV Voltage
-        self.dashboard.grid_columnconfigure(0, weight=10, uniform="equal")
-        self.dashboard.grid_rowconfigure(0, weight=3, uniform="equal")
-        self.dashboard.grid_rowconfigure(1, weight=10, uniform="equal")
+        self.headerVoltage = tk.Label(self.dashboard, text=f"GLV V", font=("Trebuchet MS", self.headerFontSize), bg="black", fg="yellow", anchor="s", padx=5, pady=5)
+        self.headerVoltage.grid(row=3, column=2, sticky="nsew", padx=(0, self.padxOut))
+        self.lblVoltage = tk.Label(self.dashboard, text=f"...", font=("Trebuchet MS", 50), bg=self.FE_green, fg="black", anchor="center", padx=5, pady=5)
+        self.lblVoltage.grid(row=4, column=2, sticky="nsew", padx=(0, self.padxOut))
 
-        self.headerVoltage = tk.Label(self.dashboard, text=f"GLV V", font=("Trebuchet MS", 40), bg="black", fg="yellow", anchor="s", padx=5, pady=5)
-        self.headerVoltage.grid(row=3, column=1, sticky="nsew", padx=(self.padxIn, self.padxOut))
-        self.lblVoltage = tk.Label(self.dashboard, text=f"...", font=("Trebuchet MS", 100), bg=FE_green, fg="black", anchor="center", padx=5, pady=5)
-        self.lblVoltage.grid(row=4, column=1, sticky="nsew", padx=(self.padxIn, self.padxOut))
-
-        self.dashboard.grid_columnconfigure(0, weight=10, uniform="equal")
-        self.dashboard.grid_rowconfigure(0, weight=3, uniform="equal")
-        self.dashboard.grid_rowconfigure(1, weight=10, uniform="equal")
         # State of Charge
-        self.headerSoC = tk.Label(self.dashboard, text=f"PACK SOCIT", font=("Trebuchet MS", 40), bg="black", fg="yellow", anchor="s", padx=5, pady=5)
-        self.headerSoC.grid(row=0, column=1, sticky="nsew", padx=(self.padxIn, self.padxOut))
-        self.lblSoC = tk.Label(self.dashboard, text=f"...", font=("Trebuchet MS", 100), bg="red", fg="black", anchor="center", padx=5, pady=5)
+        self.headerSoC = tk.Label(self.dashboard, text=f"PACK SOCIT", font=("Trebuchet MS", self.headerFontSize), bg="black", fg="yellow", anchor="s", padx=5, pady=5)
+        self.headerSoC.grid(row=0, column=2, sticky="nsew", padx=(0, self.padxOut))
+        self.lblSoC = tk.Label(self.dashboard, text=f"...", font=("Trebuchet MS", 50), bg="red", fg="black", anchor="center", padx=5, pady=5)
         # Temperature
-        self.lblSoC.grid(row=1, column=1, sticky="nsew", padx=(self.padxIn, self.padxOut), pady=(0, 20))
-        self.lblTemp = tk.Label(self.dashboard, text=f"...", font=("Trebuchet MS", 100), bg=FE_green, fg="black", anchor="center", padx=5, pady=5)
-        self.lblTemp.grid(row=2, column=1, sticky="nsew", padx=(self.padxIn, self.padxOut))
+        self.lblSoC.grid(row=1, column=2, sticky="nsew", padx=(0, self.padxOut), pady=(0,5))
+        self.lblTemp = tk.Label(self.dashboard, text=f"...", font=("Trebuchet MS", 50), bg=self.FE_green, fg="black", anchor="center", padx=5, pady=5)
+        self.lblTemp.grid(row=2, column=2, sticky="nsew", padx=(0, self.padxOut))
 
-    def updateDashboard(self): # Doesn't update until it receives CAN data
+    def updateDashboard(self):
+
+        print("Updating dashboard...")
 
         self.canbus.readMsg()
-
-        outputSpeed = str(round(self.canbus.speed))
-        outputState = self.canbus.state
-        outputVoltage = f"{(self.canbus.glvVoltage):.2f}"
-        outputSoC = f"{round(self.canbus.soc)}%"
-        outputTemp = f"{round(max(self.canbus.motorTemp, self.canbus.mcTemp, self.canbus.packTemp))}C" # Display highest temperature of motor, motor controller, battery
-
-        self.lblSpeed.config(text=outputSpeed)
-        self.lblState.config(text=outputState)
-        self.lblVoltage.config(text=outputVoltage)
-        self.lblSoC.config(text=outputSoC)
-        self.lblTemp.config(text=outputTemp)
+        
+        match self.canbus.canID:
+            case '0x500':
+                self.updateSpeed()
+            case '0x766':
+                self.updateState()
+            case '0xa9':
+                self.updateGLV()
+            case '0xa2':
+                self.updateTemp()
+            case '0xa0':
+                self.updateTemp()
+            case '0x380':
+                self.lblSoC.config(text=f"{round(self.canbus.soc)}%")
+                self.updateTemp()
 
         self.master.after(10, self.updateDashboard)
 
-root = tk.Tk()
-raspiDashboard = FE12Dashboard(root, 'vcan0', 'socketcan') # Testing using virtual can node
+    def updateState(self):
+        bmsStates = {
+            '0x4': 'BMS TEMP',
+            '0x80': 'SPI FAULT',
+            '0x8': 'OVERVOLT',
+            '0x10': 'UNDERVOLT',
+            '0x20': 'OPEN WIRE',
+            '0x40': 'MISMATCH'
+        }
 
+        try:
+            if hex(self.canbus.bmsState) in bmsStates: # Prioritize BMS faults over VCU faults
+                state = bmsStates[hex(self.canbus.vcuState)]
+                self.lblState.config(text=state, bg="red")
+            else:
+                if self.canbus.vcuState & 0x80:
+                    match self.canbus.vcuState:
+                        # case STARTUP:
+                        #     state = 'STARTUP'
+                        #     color = 'yellow'
+                        case self.DRIVE_REQUEST_FROM_LV:
+                            state = 'DRV FRM LV'
+                            color = 'red'
+                        case self.PRECHARGE_TIMEOUT:
+                            state = 'PRE TM OUT'
+                            color = 'red'
+                        case self.BRAKE_NOT_PRESSED:
+                            state = 'BR NOT PRS'
+                            color = 'red'
+                        case self.HV_DISABLED_WHILE_DRIVING:
+                            state = 'HV OFF DRV'
+                            color = 'red'
+
+
+                self.lblState.config(text=state)
+        except:
+            print(f"Invalid state encoding: {hex(self.canbus.vcuState)}")
+
+    def updateTemp(self):
+        # Display highest temperature of motor, motor controller, BMS
+
+        convMotorTemp = self.canbus.motorTemp / 10
+        convMcTemp = self.canbus.motorTemp / 10
+
+        maxTemp = max(convMotorTemp, convMcTemp, self.canbus.packTemp)
+
+        # if maxTemp == convMotorTemp or maxTemp == convMcTemp
+
+        self.lblTemp.config(text=f"{round(maxTemp)}C")
+
+    def updateSpeed(self):
+        # Slow down speed updates for readability
+
+        wheelRadius = 1302 # double-check
+        speedMPH = self.canbus.speed * 60 / wheelRadius # convert from RPM to mph
+
+        self.lblSpeed.config(text=str(round(speedMPH)))
+
+    def updateGLV(self):
+        convVoltage = self.canbus.glvVoltage / 100
+
+        if convVoltage > 10:
+            color = self.FE_green
+        elif convVoltage > 9:
+            color = "yellow"
+        else:
+            color = "red"
+        
+        self.lblVoltage.config(text=f"{(convVoltage):.2f}", bg=color)
+
+
+root = tk.Tk()
+root.attributes('-zoomed', True)
+raspiDashboard = FE12Dashboard(root, 'vcan0', 'socketcan')
+
+root.after(500, raspiDashboard.updateDashboard) # Needs time to fully load the GUI
 root.mainloop()
