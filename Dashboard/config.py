9825324bb9581173302f5f8179e52d6b4ed91b4c
Ryan Tengan <ryantengan2004@gmail.com>
2025-10-08 05:08:32 -0700

    support different OS

--- Dashboard/config.py
+++ Dashboard/config.py
@@ -1,8 +1,20 @@
-tcan = dict(
-    channel = 'vcan1',
-    interface = 'socketcan'
-)
-pcan = dict(
-    channel = 'vcan0',
-    interface = 'socketcan'
-)
+import platform
+
+if platform.system() == 'Linux':
+    tcan = dict(
+        channel = 'vcan1',
+        interface = 'socketcan'
+    )
+    pcan = dict(
+        channel = 'vcan0',
+        interface = 'socketcan'
+    )
+else:
+    tcan = dict(
+        channel = 'vcan1',
+        interface = 'virtual'
+    )
+    pcan = dict(
+        channel = 'vcan0',
+        interface = 'virtual'
+    )
